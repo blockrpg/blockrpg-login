f79e8d0bf181efcedd508e981948e4e480d13ad7
鸡毛 <shihao.gu@perfma.com>
2019-10-16 15:57:13 +0800

    注册登录接口基本实现

---
+++ src/Module/Outside/Controller/index.ts
@@ -0,0 +1,60 @@
+// 外部公开接口，主要负责玩家注册和登录功能
+// 其他接口必须要有正确的cookie才能访问
+import Router from 'koa-router';
+import * as PlayerBLL from 'blockrpg-core/built/Model/Player/BLL'
+import { Rsp } from 'blockrpg-core/built/Koa/Rsp';
+
+const router = new Router();
+
+// 注册玩家
+// 注册成功之后会返回新的玩家信息
+router.post('/api/player/register', async (ctx, next) => {
+  const params = ctx.request.body;
+  const name: string = (params.name || '').trim();
+  const password: string = (params.password || '').trim();
+  if (!name) {
+    Rsp.Fail(ctx, '请输入玩家昵称');
+    return;
+  }
+  if (name.length > 20) {
+    Rsp.Fail(ctx, '玩家昵称必须在20个字符以内');
+    return;
+  }
+  if (!password) {
+    Rsp.Fail(ctx, '请输入密码');
+    return;
+  }
+  const result = await PlayerBLL.playerRegisterBLL(name, password);
+  if (result.IsSuccess) {
+    Rsp.Success(ctx, result.Object, '注册成功');
+  } else {
+    Rsp.Fail(ctx, result.Message);
+  }
+});
+
+// 登录玩家
+// 此操作会在服务器内设置Session
+// 登录成功返回Session以及玩家信息
+// 若登录成功，此接口会通过Cookie设置Session
+router.post('/api/player/login', async (ctx, next) => {
+  const params = ctx.request.body;
+  const input: string = (params.input || '').trim();
+  const password: string = (params.password || '').trim();
+  if (!input) {
+    Rsp.Fail(ctx, '请输入玩家登录信息');
+    return;
+  }
+  if (!password) {
+    Rsp.Fail(ctx, '请输入密码');
+    return;
+  }
+  const result = await PlayerBLL.playerLoginBLL(input, password);
+  if (result.IsSuccess) {
+    ctx.cookies.set('session', result.Object.session);
+    Rsp.Success(ctx, result.Object.player, '登录成功');
+  } else {
+    Rsp.Fail(ctx, result.Message);
+  }
+});
+
+export default router.routes();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('你好，世界');
+import Koa from 'koa';
+import Router from 'koa-router';
+import KoaBodyParser from 'koa-bodyparser';
+import OutsideController from './Module/Outside/Controller';
+import { Rsp } from 'blockrpg-core/built/Koa/Rsp';
+
+const app = new Koa();
+const router = new Router();
+
+async function main() {
+  app
+    .use(KoaBodyParser())
+    .use(OutsideController)
+    .use(router.allowedMethods())
+    .use((ctx, next) => {
+      Rsp.Error(ctx, 404, '资源不存在');
+    });
+  app.listen(3000);
+}
+
+main();
