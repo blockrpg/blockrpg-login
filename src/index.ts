d30f7a37ad633ae7892cab5a270b2540b7a1813e
鸡毛 <shihao.gu@perfma.com>
2019-10-18 13:39:49 +0800

    修改名称

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import Router from 'koa-router';
 import * as BRCore from 'blockrpg-core';
 import OutsideController from './Module/Outside/Controller';
 
-const app = new BRCore.Koa.App('login', (app) => {
+const app = new BRCore.Koa.App('Login', (app) => {
   return app.use(OutsideController);
 }, false);
 
